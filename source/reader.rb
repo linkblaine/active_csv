f8cb3d3d9d81f2f191c2c9b1949a2cacea3da6e9
Zack Mance <zmance@gmail.com>
2016-06-01 12:15:27 -0400

    Adds CSV Reader Module
    The module will convert the base class filename to camelcase, then look in the
    data folder for the file. It will return an array of instances of the base class.

---
+++ source/reader.rb
@@ -0,0 +1,21 @@
+module ActiveCSV
+  module Reader
+    def build
+      filename = "data/" + camelcase + ".csv"
+      objects = []
+      CSV.foreach(filename, headers: true, header_converters: :symbol) do |row|
+        objects << self.new(row)
+      end
+
+      return objects
+    end
+
+    def camelcase
+      self.to_s.gsub(/::/, '/').
+      gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+      gsub(/([a-z\d])([A-Z])/,'\1_\2').
+      tr("-", "_").
+      downcase
+    end
+  end
+end
