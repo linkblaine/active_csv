73842c1cbcb07b3a23739262b5a870a4b3da3339
Zack Mance <zmance@gmail.com>
2016-06-01 21:03:33 -0400

    Adds Happy Path tests for search module

---
+++ spec/module_specs/active_csv_search_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper.rb'
+
+describe 'ActiveCSV::Search' do
+  let(:base_class) { SoftGood }
+  
+  describe "where" do
+    it "should return an empty array if no records found" do
+      expect(base_class.where("id", -1)).to eq []
+    end
+
+    it "should return a single object if one exists" do
+      expect(base_class.where("id", 6371).length).to eq 1
+    end
+
+    it "should return the correct record if one exists" do
+      expect(base_class.where("id", 6371).first.id).to eq "6371"
+    end
+
+    it "should return all objects that match" do 
+      expect(base_class.where("type", "pants").length).to eq 5
+    end
+  end
+end
