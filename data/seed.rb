e2be79e6d29398662ff776033e75eb114dc02d13
Zack Mance <zmance@gmail.com>
2016-06-02 14:33:57 -0400

    Adds Brands to Seed file for assications

--- data/seed.rb
+++ data/seed.rb
@@ -2,6 +2,7 @@ require 'csv'
 require 'faker'
 
 soft_good_props = %w[id type brand description]
+brand_props = %w[id name address]
 
 brands = [
   Faker::Lorem.word,
@@ -12,6 +13,13 @@ brands = [
 ]
 
 soft_goods = %w[t-shirt hoodie jacket pants gloves] 
+
+CSV.open('./data/brands.csv', 'w+') do |csv|
+  csv << brand_props
+  brands.each do |brand|
+    csv << [Faker::Number.number(4), brand, Faker::Address.street_address]
+  end
+end
 
 CSV.open('./data/soft_goods.csv', 'w+') do |csv|
   csv << soft_good_props
